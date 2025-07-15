f765bb6a8ce21877ea5ec9fe76af54a8b1942d45
sally__cd <88639700+cullMeSal@users.noreply.github.com>
2025-07-15 23:35:52 +0700

    fixed error with flyway data initialization

--- src/main/java/demo02/demo/dto/UserAuthDTO.java
+++ src/main/java/demo02/demo/dto/UserAuthDTO.java
@@ -6,4 +6,6 @@ import lombok.Data;
 public class UserAuthDTO {
     String username;
     String password;
+
+
 }
--- src/main/resources/dataaaa.sql
+++
@@ -1,6 +0,0 @@
-
-INSERT INTO users (username, password, email)
-    VALUES
-        ('admin1', '1', 'admin@local.host'),
-        ('user1', '12345', 'user1@local.host'),
-        ('user2', '123456a@', 'userZWEI@localh.ost');
